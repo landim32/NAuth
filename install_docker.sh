b77d62ece993815ea4605fc278a8f78a643a4a4c
Rodrigo <rodrigo@emagine.com.br>
2025-06-07 05:32:22 -0300

    add prod

--- install_docker.sh
+++ install_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker stop nauth-api1
 docker rm nauth-api1
-docker build -t nauth-api -f NAuth.API/Dockerfile .
+cd ./Backend/NAuth.API
+docker build -t nauth-api -f Dockerfile .
 docker run --name nauth-api1 -e ASPNETCORE_URLS="https://+" -e ASPNETCORE_HTTPS_PORTS=443 --network docker-network nauth-api &
 docker ps
