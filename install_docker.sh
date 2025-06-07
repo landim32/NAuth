5ffdb719f1094ef7da0568f3ca7d0342ea4e4495
Rodrigo <rodrigo@emagine.com.br>
2025-06-07 05:47:22 -0300

    fix deploy

--- install_docker.sh
+++ install_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#docker stop nauth-api1
-#docker rm nauth-api1
+docker stop nauth-api1
+docker rm nauth-api1
 cd ./Backend/NAuth/NAuth.API
 docker build -t nauth-api -f Dockerfile .
 docker run --name nauth-api1 -e ASPNETCORE_URLS="https://+" -e ASPNETCORE_HTTPS_PORTS=443 --network docker-network nauth-api &
