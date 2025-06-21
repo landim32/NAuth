6a130d0a0a2b7bf5d3682b7d37ae7b478fddc862
rodrigo.carneiro <rodrigo.carneiro@benner.com.br>
2025-06-21 18:32:55 -0300

    Fix AuthHandler

---
+++ Backend/NAuth/NAuth.DTO/Client/ClientInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NAuth.DTO.Client
+{
+    public class ClientInfo
+    {
+        [JsonPropertyName("clientId")]
+        public long ClientId { get; set; }
+        [JsonPropertyName("userId")]
+        public long UserId { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ Backend/NAuth/NAuth.DTO/Client/ClientResult.cs
@@ -0,0 +1,16 @@
+﻿using NAuth.DTO.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NAuth.DTO.Client
+{
+    public class ClientResult: StatusResult
+    {
+        [JsonPropertyName("value")]
+        public ClientInfo Value { get; set; }
+    }
+}
