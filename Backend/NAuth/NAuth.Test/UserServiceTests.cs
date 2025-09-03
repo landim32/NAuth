40288a1eae0d2e6f5f5cde821053979d00a389be
Rodrigo Landim Carneiro <rodrigo.carneiro@benner.com.br>
2025-09-03 14:27:17 -0300

    test: cover ListUsers

--- Backend/NAuth/NAuth.Test/UserServiceTests.cs
+++ Backend/NAuth/NAuth.Test/UserServiceTests.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Moq;
 using Xunit;
 using NAuth.Domain.Impl.Services;
 using NAuth.Domain.Interfaces.Factory;
 using NAuth.Domain.Interfaces.Models;
 using NAuth.Domain.Interfaces.Services;
+using NAuth.DTO.MailerSend;
+using NAuth.DTO.User;
 
 namespace NAuth.Test;
 
@@ -32,6 +36,213 @@ public class UserServiceTests
             tokenFactory.Object,
             mailer.Object,
             image.Object);
+    }
+
+    [Fact]
+    public void LoginWithEmail_CallsModelAndReturnsResult()
+    {
+        var userModel = new Mock<IUserModel>();
+        var loggedUser = new Mock<IUserModel>();
+        var userFactory = new Mock<IUserDomainFactory>();
+
+        userFactory.Setup(f => f.BuildUserModel()).Returns(userModel.Object);
+        userModel.Setup(m => m.LoginWithEmail("mail@test.com", "pwd", userFactory.Object))
+                  .Returns(loggedUser.Object);
+
+        var service = CreateService(userFactory: userFactory);
+
+        var result = service.LoginWithEmail("mail@test.com", "pwd");
+
+        userFactory.Verify(f => f.BuildUserModel(), Times.Once);
+        userModel.Verify(m => m.LoginWithEmail("mail@test.com", "pwd", userFactory.Object), Times.Once);
+        Assert.Same(loggedUser.Object, result);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void HasPassword_CallsModelAndReturnsResult(bool hasPassword)
+    {
+        var userModel = new Mock<IUserModel>();
+        var userFactory = new Mock<IUserDomainFactory>();
+
+        userFactory.Setup(f => f.BuildUserModel()).Returns(userModel.Object);
+        userModel.Setup(m => m.HasPassword(5, userFactory.Object)).Returns(hasPassword);
+
+        var service = CreateService(userFactory: userFactory);
+
+        var result = service.HasPassword(5);
+
+        userFactory.Verify(f => f.BuildUserModel(), Times.Once);
+        userModel.Verify(m => m.HasPassword(5, userFactory.Object), Times.Once);
+        Assert.Equal(hasPassword, result);
+    }
+
+    [Fact]
+    public void GetUserByEmail_CallsModelAndReturnsResult()
+    {
+        var userModel = new Mock<IUserModel>();
+        var userFactory = new Mock<IUserDomainFactory>();
+        userFactory.Setup(f => f.BuildUserModel()).Returns(userModel.Object);
+        userModel.Setup(m => m.GetByEmail("mail@test.com", userFactory.Object)).Returns(userModel.Object);
+
+        var service = CreateService(userFactory: userFactory);
+
+        var result = service.GetUserByEmail("mail@test.com");
+
+        userFactory.Verify(f => f.BuildUserModel(), Times.Once);
+        userModel.Verify(m => m.GetByEmail("mail@test.com", userFactory.Object), Times.Once);
+        Assert.Same(userModel.Object, result);
+    }
+
+    [Fact]
+    public void GetUserByToken_TokenNotFound_ReturnsNull()
+    {
+        var tokenModel = new Mock<IUserTokenModel>();
+        var tokenFactory = new Mock<IUserTokenDomainFactory>();
+        tokenFactory.Setup(f => f.BuildUserTokenModel()).Returns(tokenModel.Object);
+        tokenModel.Setup(m => m.GetByToken("token123", tokenFactory.Object)).Returns((IUserTokenModel?)null);
+
+        var userFactory = new Mock<IUserDomainFactory>();
+
+        var service = CreateService(userFactory: userFactory, tokenFactory: tokenFactory);
+
+        var result = service.GetUserByToken("token123");
+
+        tokenFactory.Verify(f => f.BuildUserTokenModel(), Times.Once);
+        tokenModel.Verify(m => m.GetByToken("token123", tokenFactory.Object), Times.Once);
+        userFactory.Verify(f => f.BuildUserModel(), Times.Never);
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void GetUserByToken_TokenFound_ReturnsUser()
+    {
+        var tokenModel = new Mock<IUserTokenModel>();
+        tokenModel.SetupGet(t => t.UserId).Returns(5);
+        var tokenFactory = new Mock<IUserTokenDomainFactory>();
+        tokenFactory.Setup(f => f.BuildUserTokenModel()).Returns(tokenModel.Object);
+        tokenModel.Setup(m => m.GetByToken("token123", tokenFactory.Object)).Returns(tokenModel.Object);
+
+        var userModel = new Mock<IUserModel>();
+        var userFactory = new Mock<IUserDomainFactory>();
+        userFactory.Setup(f => f.BuildUserModel()).Returns(userModel.Object);
+        userModel.Setup(m => m.GetById(5, userFactory.Object)).Returns(userModel.Object);
+
+        var service = CreateService(userFactory: userFactory, tokenFactory: tokenFactory);
+
+        var result = service.GetUserByToken("token123");
+
+        tokenFactory.Verify(f => f.BuildUserTokenModel(), Times.Once);
+        tokenModel.Verify(m => m.GetByToken("token123", tokenFactory.Object), Times.Once);
+        userFactory.Verify(f => f.BuildUserModel(), Times.Once);
+        userModel.Verify(m => m.GetById(5, userFactory.Object), Times.Once);
+        Assert.Same(userModel.Object, result);
+    }
+
+    [Fact]
+    public void GetByStripeId_CallsModelAndReturnsResult()
+    {
+        var userModel = new Mock<IUserModel>();
+        var userFactory = new Mock<IUserDomainFactory>();
+        userFactory.Setup(f => f.BuildUserModel()).Returns(userModel.Object);
+        userModel.Setup(m => m.GetByStripeId("stripe_123", userFactory.Object)).Returns(userModel.Object);
+
+        var service = CreateService(userFactory: userFactory);
+
+        var result = service.GetByStripeId("stripe_123");
+
+        userFactory.Verify(f => f.BuildUserModel(), Times.Once);
+        userModel.Verify(m => m.GetByStripeId("stripe_123", userFactory.Object), Times.Once);
+        Assert.Same(userModel.Object, result);
+    }
+
+    [Fact]
+    public void GetBySlug_CallsModelAndReturnsResult()
+    {
+        var userModel = new Mock<IUserModel>();
+        var userFactory = new Mock<IUserDomainFactory>();
+        userFactory.Setup(f => f.BuildUserModel()).Returns(userModel.Object);
+        userModel.Setup(m => m.GetBySlug("sluggy", userFactory.Object)).Returns(userModel.Object);
+
+        var service = CreateService(userFactory: userFactory);
+
+        var result = service.GetBySlug("sluggy");
+
+        userFactory.Verify(f => f.BuildUserModel(), Times.Once);
+        userModel.Verify(m => m.GetBySlug("sluggy", userFactory.Object), Times.Once);
+        Assert.Same(userModel.Object, result);
+    }
+
+    [Fact]
+    public void ListUsers_CallsModelAndReturnsResult()
+    {
+        var userModel = new Mock<IUserModel>();
+        var userFactory = new Mock<IUserDomainFactory>();
+        userFactory.Setup(f => f.BuildUserModel()).Returns(userModel.Object);
+
+        var user1 = new Mock<IUserModel>().Object;
+        var user2 = new Mock<IUserModel>().Object;
+        var users = new List<IUserModel> { user1, user2 };
+        userModel.Setup(m => m.ListUsers(2, userFactory.Object)).Returns(users);
+
+        var service = CreateService(userFactory: userFactory);
+
+        var result = service.ListUsers(2);
+
+        userFactory.Verify(f => f.BuildUserModel(), Times.Once);
+        userModel.Verify(m => m.ListUsers(2, userFactory.Object), Times.Once);
+        Assert.Collection(result,
+            u => Assert.Same(user1, u),
+            u => Assert.Same(user2, u));
+    }
+
+    [Fact]
+    public void ChangePassword_HasPassword_VerifiesOldPasswordAndUpdates()
+    {
+        var userModel = new Mock<IUserModel>();
+        var userFactory = new Mock<IUserDomainFactory>();
+        userFactory.Setup(f => f.BuildUserModel()).Returns(userModel.Object);
+
+        userModel.Setup(m => m.HasPassword(10, userFactory.Object)).Returns(true);
+
+        var existingUser = new Mock<IUserModel>();
+        existingUser.SetupGet(u => u.UserId).Returns(10);
+        existingUser.SetupGet(u => u.Email).Returns("mail@test.com");
+        userModel.Setup(m => m.GetById(10, userFactory.Object)).Returns(existingUser.Object);
+        userModel.Setup(m => m.LoginWithEmail("mail@test.com", "oldpwd", userFactory.Object)).Returns(existingUser.Object);
+
+        var service = CreateService(userFactory: userFactory);
+
+        service.ChangePassword(10, "oldpwd", "newpwd");
+
+        userFactory.Verify(f => f.BuildUserModel(), Times.Exactly(2));
+        userModel.Verify(m => m.HasPassword(10, userFactory.Object), Times.Once);
+        userModel.Verify(m => m.GetById(10, userFactory.Object), Times.Once);
+        userModel.Verify(m => m.LoginWithEmail("mail@test.com", "oldpwd", userFactory.Object), Times.Once);
+        userModel.Verify(m => m.ChangePassword(10, "newpwd", userFactory.Object), Times.Once);
+    }
+
+    [Fact]
+    public void ChangePassword_NoExistingPassword_UpdatesDirectly()
+    {
+        var userModel = new Mock<IUserModel>();
+        var userFactory = new Mock<IUserDomainFactory>();
+        userFactory.Setup(f => f.BuildUserModel()).Returns(userModel.Object);
+
+        userModel.Setup(m => m.HasPassword(10, userFactory.Object)).Returns(false);
+
+        var existingUser = new Mock<IUserModel>();
+        existingUser.SetupGet(u => u.UserId).Returns(10);
+        existingUser.SetupGet(u => u.Email).Returns("mail@test.com");
+        userModel.Setup(m => m.GetById(10, userFactory.Object)).Returns(existingUser.Object);
+
+        var service = CreateService(userFactory: userFactory);
+
+        service.ChangePassword(10, string.Empty, "newpwd");
+
+        userModel.Verify(m => m.LoginWithEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<IUserDomainFactory>()), Times.Never);
+        userModel.Verify(m => m.ChangePassword(10, "newpwd", userFactory.Object), Times.Once);
     }
 
     [Theory]
@@ -96,6 +307,160 @@ public class UserServiceTests
         service.ChangePasswordUsingHash("hash", "newpass");
 
         userModel.Verify(m => m.ChangePassword(10, "newpass", userFactory.Object), Times.Once);
+    }
+
+    [Fact]
+    public async Task SendRecoveryEmail_UserNotFound_ThrowsException()
+    {
+        var userModel = new Mock<IUserModel>();
+        var userFactory = new Mock<IUserDomainFactory>();
+        userFactory.Setup(f => f.BuildUserModel()).Returns(userModel.Object);
+        userModel.Setup(m => m.GetByEmail("mail@test.com", userFactory.Object)).Returns((IUserModel?)null);
+
+        var service = CreateService(userFactory: userFactory);
+
+        await Assert.ThrowsAsync<Exception>(() => service.SendRecoveryEmail("mail@test.com"));
+    }
+
+    [Fact]
+    public async Task SendRecoveryEmail_Valid_SendsEmailAndReturnsTrue()
+    {
+        var userModel = new Mock<IUserModel>();
+        var userFactory = new Mock<IUserDomainFactory>();
+        userFactory.Setup(f => f.BuildUserModel()).Returns(userModel.Object);
+
+        var existingUser = new Mock<IUserModel>();
+        existingUser.SetupGet(u => u.UserId).Returns(10);
+        existingUser.SetupGet(u => u.Email).Returns("mail@test.com");
+        existingUser.SetupGet(u => u.Name).Returns("Tester");
+        userModel.Setup(m => m.GetByEmail("mail@test.com", userFactory.Object)).Returns(existingUser.Object);
+        userModel.Setup(m => m.GenerateRecoveryHash(10, userFactory.Object)).Returns("hash123");
+
+        var mailer = new Mock<IMailerSendService>();
+        mailer.Setup(m => m.Sendmail(It.IsAny<MailerInfo>())).ReturnsAsync(true);
+
+        var service = CreateService(userFactory: userFactory, mailer: mailer);
+
+        var result = await service.SendRecoveryEmail("mail@test.com");
+
+        userFactory.Verify(f => f.BuildUserModel(), Times.Once);
+        userModel.Verify(m => m.GetByEmail("mail@test.com", userFactory.Object), Times.Once);
+        userModel.Verify(m => m.GenerateRecoveryHash(10, userFactory.Object), Times.Once);
+        mailer.Verify(m => m.Sendmail(It.Is<MailerInfo>(mail =>
+            mail.Subject == "[NoChainSwap] Password Recovery Email" &&
+            mail.To.Count == 1 &&
+            mail.To[0].Email == "mail@test.com" &&
+            mail.Html.Contains("https://nochainswap.org/recoverypassword/hash123"))), Times.Once);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Insert_ValidUser_InsertsModelAndRelatedData()
+    {
+        var userModel = new Mock<IUserModel>();
+        userModel.SetupAllProperties();
+        userModel.Setup(m => m.Insert(It.IsAny<IUserDomainFactory>())).Returns(userModel.Object);
+        userModel.Setup(m => m.GetByEmail("mail@test.com", It.IsAny<IUserDomainFactory>())).Returns((IUserModel?)null);
+        userModel.Setup(m => m.ExistSlug(It.IsAny<long>(), It.IsAny<string>())).Returns(false);
+        userModel.Object.UserId = 42;
+
+        var userFactory = new Mock<IUserDomainFactory>();
+        userFactory.Setup(f => f.BuildUserModel()).Returns(userModel.Object);
+
+        var phoneModel = new Mock<IUserPhoneModel>();
+        phoneModel.SetupAllProperties();
+        phoneModel.Setup(m => m.Insert(It.IsAny<IUserPhoneDomainFactory>())).Returns(phoneModel.Object);
+        var phoneFactory = new Mock<IUserPhoneDomainFactory>();
+        phoneFactory.Setup(f => f.BuildUserPhoneModel()).Returns(phoneModel.Object);
+
+        var addrModel = new Mock<IUserAddressModel>();
+        addrModel.SetupAllProperties();
+        addrModel.Setup(m => m.Insert(It.IsAny<IUserAddressDomainFactory>())).Returns(addrModel.Object);
+        var addrFactory = new Mock<IUserAddressDomainFactory>();
+        addrFactory.Setup(f => f.BuildUserAddressModel()).Returns(addrModel.Object);
+
+        var service = CreateService(userFactory: userFactory, phoneFactory: phoneFactory, addrFactory: addrFactory);
+
+        var user = new UserInfo
+        {
+            Name = "Tester",
+            Email = "mail@test.com",
+            Password = "pwd",
+            Phones = new List<UserPhoneInfo> { new UserPhoneInfo { Phone = "123456789" } },
+            Addresses = new List<UserAddressInfo> { new UserAddressInfo { ZipCode = "12345678", Address = "Street", Complement = "Comp", Neighborhood = "Neigh", City = "City", State = "ST" } }
+        };
+
+        var result = service.Insert(user);
+
+        userFactory.Verify(f => f.BuildUserModel(), Times.Once);
+        userModel.Verify(m => m.GetByEmail("mail@test.com", userFactory.Object), Times.Once);
+        userModel.Verify(m => m.Insert(userFactory.Object), Times.Once);
+        userModel.Verify(m => m.ChangePassword(42, "pwd", userFactory.Object), Times.Once);
+        phoneFactory.Verify(f => f.BuildUserPhoneModel(), Times.Once);
+        phoneModel.Verify(m => m.Insert(phoneFactory.Object), Times.Once);
+        addrFactory.Verify(f => f.BuildUserAddressModel(), Times.Once);
+        addrModel.Verify(m => m.Insert(addrFactory.Object), Times.Once);
+        Assert.Same(userModel.Object, result);
+        Assert.Equal(42, phoneModel.Object.UserId);
+        Assert.Equal("123456789", phoneModel.Object.Phone);
+        Assert.Equal(42, addrModel.Object.UserId);
+    }
+
+    [Fact]
+    public void Update_ValidUser_UpdatesModelAndRelatedData()
+    {
+        var userModel = new Mock<IUserModel>();
+        userModel.SetupAllProperties();
+        userModel.Setup(m => m.GetById(42, It.IsAny<IUserDomainFactory>())).Returns(userModel.Object);
+        userModel.Setup(m => m.GetByEmail("mail@test.com", It.IsAny<IUserDomainFactory>())).Returns((IUserModel?)null);
+        userModel.Setup(m => m.Update(It.IsAny<IUserDomainFactory>())).Returns(userModel.Object);
+        userModel.Setup(m => m.ExistSlug(It.IsAny<long>(), It.IsAny<string>())).Returns(false);
+        userModel.Object.UserId = 42;
+
+        var userFactory = new Mock<IUserDomainFactory>();
+        userFactory.Setup(f => f.BuildUserModel()).Returns(userModel.Object);
+
+        var phoneModel = new Mock<IUserPhoneModel>();
+        phoneModel.SetupAllProperties();
+        phoneModel.Setup(m => m.Insert(It.IsAny<IUserPhoneDomainFactory>())).Returns(phoneModel.Object);
+        phoneModel.Setup(m => m.DeleteAllByUser(42));
+        var phoneFactory = new Mock<IUserPhoneDomainFactory>();
+        phoneFactory.Setup(f => f.BuildUserPhoneModel()).Returns(phoneModel.Object);
+
+        var addrModel = new Mock<IUserAddressModel>();
+        addrModel.SetupAllProperties();
+        addrModel.Setup(m => m.Insert(It.IsAny<IUserAddressDomainFactory>())).Returns(addrModel.Object);
+        addrModel.Setup(m => m.DeleteAllByUser(42));
+        var addrFactory = new Mock<IUserAddressDomainFactory>();
+        addrFactory.Setup(f => f.BuildUserAddressModel()).Returns(addrModel.Object);
+
+        var service = CreateService(userFactory: userFactory, phoneFactory: phoneFactory, addrFactory: addrFactory);
+
+        var user = new UserInfo
+        {
+            UserId = 42,
+            Name = "Tester",
+            Email = "mail@test.com",
+            Phones = new List<UserPhoneInfo> { new UserPhoneInfo { Phone = "123456789" } },
+            Addresses = new List<UserAddressInfo> { new UserAddressInfo { ZipCode = "12345678", Address = "Street", Complement = "Comp", Neighborhood = "Neigh", City = "City", State = "ST" } }
+        };
+
+        var result = service.Update(user);
+
+        userFactory.Verify(f => f.BuildUserModel(), Times.Once);
+        userModel.Verify(m => m.GetById(42, userFactory.Object), Times.Once);
+        userModel.Verify(m => m.GetByEmail("mail@test.com", userFactory.Object), Times.Once);
+        userModel.Verify(m => m.Update(userFactory.Object), Times.Once);
+        phoneFactory.Verify(f => f.BuildUserPhoneModel(), Times.Exactly(2));
+        phoneModel.Verify(m => m.DeleteAllByUser(42), Times.Once);
+        phoneModel.Verify(m => m.Insert(phoneFactory.Object), Times.Once);
+        addrFactory.Verify(f => f.BuildUserAddressModel(), Times.Exactly(2));
+        addrModel.Verify(m => m.DeleteAllByUser(42), Times.Once);
+        addrModel.Verify(m => m.Insert(addrFactory.Object), Times.Once);
+        Assert.Same(userModel.Object, result);
+        Assert.Equal(42, phoneModel.Object.UserId);
+        Assert.Equal("123456789", phoneModel.Object.Phone);
+        Assert.Equal(42, addrModel.Object.UserId);
     }
 }
 
